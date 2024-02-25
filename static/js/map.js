9bf2071829c7565a9072edf0daeecf7bd8c1cd55
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-02-25 14:59:42 +0700

    revamp

---
+++ static/js/map.js
@@ -0,0 +1,45 @@
+var map;
+
+// Check if there are locations available
+if (locations.length > 0) {
+  var firstLocation = locations[0];
+
+  // Initialize the map and set view to the first location
+  map = L.map("map").setView([firstLocation.lat, firstLocation.long], 13);
+} else {
+  // Initialize the map with default view
+  map = L.map("map").setView([51.505, -0.09], 13);
+}
+
+// Add a base layer to the map
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+}).addTo(map);
+
+// Extract latitude and longitude from locations array
+var latlngs = locations.map(function (location) {
+  return [location.lat, location.long];
+});
+
+// Add marker for the first location
+if (locations.length > 0) {
+  var firstLocation = locations[0];
+  L.marker([firstLocation.lat, firstLocation.long])
+    .addTo(map)
+    .bindPopup("Latitude: " + firstLocation.lat + "<br>Longitude: " + firstLocation.long)
+    .openPopup();
+}
+
+// Add marker for the last location
+if (locations.length > 1) {
+  var lastLocation = locations[locations.length - 1];
+  L.marker([lastLocation.lat, lastLocation.long])
+    .addTo(map)
+    .bindPopup("Latitude: " + lastLocation.lat + "<br>Longitude: " + lastLocation.long)
+    .openPopup();
+}
+
+// Create a polyline with dashed lines, starting from the second location
+if (locations.length > 1) {
+  var polyline = L.polyline(latlngs.slice(1), { dashArray: "10, 10" }).addTo(map);
+}
