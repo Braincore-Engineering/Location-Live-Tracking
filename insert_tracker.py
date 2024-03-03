17d50bb4692f534df216a1d3048deb37d4606d4d
dzackiero <dzakynashshar@gmail.com>
2024-03-03 23:17:14 +0700

    feat: make form for inserting tracker

--- insert_tracker.py
+++ insert_tracker.py
@@ -1,16 +1,31 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, redirect, render_template, request, url_for
 from database.model.tracker import Tracker
 from database.mysql import db, format_database_error
-from auth import auth
-from rate_limiter import limiter
+from form.form_insert import InsertTrackerForm
 
 bp = Blueprint("mysql", __name__, url_prefix="/tracker")
 
 
-@bp.route("/insert/", methods=["POST"])
-@limiter.limit("5 per minute")
-@auth.login_required()
+@bp.route("/insert/", methods=["POST", "GET"])
 def insert_data():
+    form = InsertTrackerForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        desc = form.name.description
+
+        tracker = Tracker(name=name, description=desc)
+        db.session.add(tracker)
+        db.session.commit()
+
+        return redirect(url_for("location_live_tracking.location_live_tracking"))
+    return render_template(
+        "pages/location_live_tracking/insert_tracker.html", form=form
+    )
+
+
+@bp.route("/api/insert/", methods=["POST"])
+def insert_data_api():
     name = request.form.get("name")
     desc = request.form.get("description")
 
