88d419174220d43eb1f887a43d5d31817381b871
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-03-04 00:56:30 +0700

    ngubah env

--- config.py
+++ config.py
@@ -1,21 +1,20 @@
-from dotenv import dotenv_values
+import os
 
-config = dotenv_values(".env")
 
 CONFIG = {
-    "SECRET_KEY": config["SECRET_KEY"],
+    "SECRET_KEY": os.getenv("SECRET_KEY"),
     "CACHE_TYPE": "SimpleCache",
     "CACHE_DEFAULT_TIMEOUT": 300,
     "SESSION_TYPE": "filesystem",
     "SQLALCHEMY_DATABASE_URI": (
         "mysql+pymysql://"
-        + config["MYSQL_USERNAME"]
+        + os.getenv("MYSQL_USERNAME")
         + ":"
-        + config["MYSQL_PASSWORD"]
+        + os.getenv("MYSQL_PASSWORD")
         + "@"
-        + config["MYSQL_HOST"]
+        + os.getenv("MYSQL_HOST")
         + "/"
-        + config["MYSQL_DATABASE"]
+        + os.getenv("MYSQL_DATABASE")
     ),
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
 }
