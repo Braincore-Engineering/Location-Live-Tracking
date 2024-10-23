b85f0f16f7123c316fa203de998e34bf28ada077
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-10-23 17:26:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,20 @@
 import os
-# from dotenv import load_dotenv
 
-# load_dotenv()
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+MYSQL_USERNAME = os.getenv("MYSQL_USERNAME")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+
+# Debugging: Print values to ensure they're set
+print("MYSQL_USERNAME:", MYSQL_USERNAME)
+print("MYSQL_PASSWORD:", MYSQL_PASSWORD)
+print("MYSQL_HOST:", MYSQL_HOST)
+print("MYSQL_DATABASE:", MYSQL_DATABASE)
 
 CONFIG = {
     "SECRET_KEY": os.getenv("SECRET_KEY"),
@@ -10,13 +23,13 @@ CONFIG = {
     "SESSION_TYPE": "filesystem",
     "SQLALCHEMY_DATABASE_URI": (
         "mysql+pymysql://"
-        + os.getenv("MYSQL_USERNAME")
+        + MYSQL_USERNAME
         + ":"
-        + os.getenv("MYSQL_PASSWORD")
+        + MYSQL_PASSWORD
         + "@"
-        + os.getenv("MYSQL_HOST")
+        + MYSQL_HOST
         + "/"
-        + os.getenv("MYSQL_DATABASE")
+        + MYSQL_DATABASE
     ),
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
 }
